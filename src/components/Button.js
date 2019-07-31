07644689fae973f943dea50bfb8a9c5680f01b0a
Nikolaos Antonakakis <n.antonakakis@gmail.com>
2019-07-31 10:00:52 +0200

    7. Creating Context Objects

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,17 @@
 import React from "react";
+import LanguageContext from "../contexts/LanguageContext";
 
 class Button extends React.Component {
+  //this is how we hook up a context object to a class component
+  static contextType = LanguageContext;
+
   render() {
+    console.log(this.context);
     return <button className="ui button primary">Submit</button>;
   }
 }
+
+// Alternative way to the hook up above:
+// Button.contextType = LanguageContext;
 
 export default Button;
